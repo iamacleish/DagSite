14ae59dfa6af7f2ffbe3c21b04b0aa3a9396ad8f
imacleis <imacleis@ucsc.edu>
2017-05-17 01:47:54 -0700

    built most of the site. need permission for pictures/other pictures.

--- public/js/mycode.js
+++ public/js/mycode.js
@@ -1,19 +1,1 @@
-$(document).ready(function(){
-    $(".toggleList").click(function(){
-      var target = "#list" + $(this).attr("id");
-      $(target).toggle();
-    });
-});
 
-
-document.addEventListener("deviceready", onDeviceReady, false);
-function onDeviceReady() {
-    console.log("ready");
-    console.log(cordova.file);
-}
-
-function backToIndex()
-{
-  console.log(cordova.file.applicationDirectory+'www/index.html');
-  window.location.href = cordova.file.applicationDirectory+'www/index.html';
-}
